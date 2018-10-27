a2dd1bb332ee517aa10c333f05b73e3cc8804eb8
vu <mainguyenvu210@gmail.com>
2018-10-27 16:18:45 +0700

    can crawl entire domain

--- rust/spiders/rust_spider.py
+++ rust/spiders/rust_spider.py
@@ -1,3 +1,4 @@
+import csv
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 
@@ -5,6 +6,8 @@ from scrapy.linkextractors import LinkExtractor
 class RustSpider(scrapy.Spider):
     name = "rust"
     export_urls = []
+
+    # add domain here
     allowed_domains = ["stream-hub.com"]
     start_urls = (
         'http://stream-hub.com/',
@@ -13,10 +16,22 @@ class RustSpider(scrapy.Spider):
     def parse(self, response):
         extractor = LinkExtractor(allow_domains='stream-hub.com')
         links = extractor.extract_links(response)
-        for link in links:
-            if(link.url not in self.export_urls):
-                self.export_urls.append(link.url)
+        items = []
+        count = 0
+        with open('all_links.csv','a') as file:
+            for link in links:
+                # write to last row
+                if(link.url not in self.export_urls):
+                    # write to file
+                    file.write(link.url)
 
-        print(self.export_urls)
+                    # insert new line
+                    file.write('\n')
 
-        yield response.follow(links[1], callback=self.parse)
+                    # add to existing array
+                    self.export_urls.append(link.url)
+
+                    # go to next link
+                    yield response.follow(link.url, callback=self.parse)
+
+        

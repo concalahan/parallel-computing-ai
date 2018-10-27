5de9b051ac642873afb95de5c2e2045a48977944
vu <mainguyenvu210@gmail.com>
2018-10-27 16:55:59 +0700

    can parse file

---
+++ parser.py
@@ -0,0 +1,60 @@
+# encoding=utf8
+
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
+from urllib import parse
+import sys
+import os, os.path
+import urllib
+
+sys.path.append(os.path.realpath('..'))
+
+# save to this directory
+READ_DIR = './data/url/'
+
+WRITE_DIR = './data/raw/'
+
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print('Create directory : ' + directory)
+        os.makedirs(directory)
+    else:
+        print(directory + ' is already created !')
+
+
+def writeFile(provider, soup, i):
+    with open(WRITE_DIR + provider + "_" + str(i) + '.html', 'w', encoding='utf-8') as the_file:
+        the_file.write(str(soup))
+
+
+def parser_main():
+    # create the directory if not empty
+    create_project_dir(WRITE_DIR)
+
+    headers = {'User-Agent': 'User-Agent:Mozilla/5.0'}
+
+    with open(READ_DIR + "all_links.csv") as fp:
+        i = 0
+
+        for url in fp:
+            print("Processing file " + str(url))
+
+            try:
+                dataTemp = urllib.request.Request(url, headers=headers)
+                data = urllib.request.urlopen(dataTemp).read()
+                soup = BeautifulSoup(data, "lxml")
+                writeFile('stream', soup, i)
+
+                i += 1
+            except HTTPError as e:
+                # do something
+                print('Error code: ', e.code)
+            except URLError as e:
+                # do something
+                print('Reason: ', e.reason)
+
+
+if __name__ == "__main__":
+    parser_main()
--- rust/spiders/rust_spider.py
+++ rust/spiders/rust_spider.py
@@ -1,10 +1,20 @@
+# encoding=utf8
+
 import csv
 import scrapy
 from scrapy.linkextractors import LinkExtractor
+import sys
+import os, os.path
+import re
 
+sys.path.append(os.path.realpath('..'))
+
+# save to this directory
+WRITE_DIR = '../data/url/'
 
 class RustSpider(scrapy.Spider):
     name = "rust"
+    isCreateFolder = False
     export_urls = []
 
     # add domain here
@@ -13,25 +23,50 @@ class RustSpider(scrapy.Spider):
         'http://stream-hub.com/',
     )
 
+    def create_project_dir(self, directory):
+        # delete file if exist
+        if os.path.exists(WRITE_DIR + 'all_links.csv'):
+            os.remove(WRITE_DIR + 'all_links.csv')
+        else:
+            print("The file does not exist") 
+
+        # create folder if not exist
+        if not os.path.exists(directory):
+            print('Create directory : ' + directory)
+            os.makedirs(directory)
+        else:
+            print(directory + ' is already created !')
+
     def parse(self, response):
+        
+        if(self.isCreateFolder == False):
+            # create save data directory once
+            self.create_project_dir(WRITE_DIR)
+            self.isCreateFolder = True
+
         extractor = LinkExtractor(allow_domains='stream-hub.com')
         links = extractor.extract_links(response)
         items = []
-        count = 0
-        with open('all_links.csv','a') as file:
+        
+        with open(WRITE_DIR + 'all_links.csv', 'a') as file:
+            # write to last row
             for link in links:
-                # write to last row
-                if(link.url not in self.export_urls):
+                # remove stupid character in url
+                tempLink = link.url.split('#', 1)[0]
+                
+                if(tempLink not in self.export_urls):
+                    print("Process " + tempLink)
+
                     # write to file
-                    file.write(link.url)
+                    file.write(tempLink)
 
                     # insert new line
                     file.write('\n')
 
                     # add to existing array
-                    self.export_urls.append(link.url)
+                    self.export_urls.append(tempLink)
 
                     # go to next link
-                    yield response.follow(link.url, callback=self.parse)
+                    yield response.follow(tempLink, callback=self.parse)
 
         
